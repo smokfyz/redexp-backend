e1305b3d61f146d48b1ee6a1189c5f28144776e3
smokfyz <ivansharapenkov@gmail.com>
2018-02-04 21:31:10 +0300

    Setting app for use session

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
+const session = require('express-session');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./config/db')
 const app = express();
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+  secret: 'hello world',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.get('/', (req, res) => {
+  console.log(req.sessionID);
+  res.sendFile(__dirname + '/index.html');
+});
 
 MongoClient.connect(db.url, (err, database) => {
 
   if(err) return console.log(err);
   require('./app/routes')(app, database.db('redexp'));
 
-  app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
-  });
-
   app.listen(3000, () => {
     console.log('Listening on port 3000');
   });
-  
+
 });
